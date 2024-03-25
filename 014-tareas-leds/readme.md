711284c6c9b5dfd8d0c3dc6168311fd2a147df52
moisesfa <moifdez@gmail.com>
2024-03-25 13:10:27 +0100

    creación de tareas básico

---
+++ 014-tareas-leds/readme.md
@@ -0,0 +1,14 @@
+# Estudio FreeRTOS Tareas
+
+[FreeRTOS TASK](https://www.youtube.com/watch?v=Vus7HE3wc6A&list=PL-Hb9zZP9qC65SpXHnTAO0-qV6x5JxCMJ&index=10)
+
+## Temas estudiados
+
+- [x] Uso de tareas básico. 
+
+
+<div style="text-align: center;">
+
+![](docs/result.png)
+
+</div>
---
+++ 014-tareas-leds/src/main.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+#define ledR 16
+#define ledG 17
+#define ledB 5
+#define STACK_SIZE 1024*2
+
+#define R_DELAY 1000
+#define G_DELAY 2000
+#define B_DELAY 4000
+
+const char *TAG = "Tareas";
+
+esp_err_t init_led(void);
+esp_err_t create_tasks(void);
+
+void vTaskLedR(void *pvParameters);
+void vTaskLedG(void *pvParameters);
+void vTaskLedB(void *pvParameters);
+
+void app_main()
+{
+    init_led();
+    create_tasks();
+    while (1)
+    {
+        // gpio_set_level(ledR, 1);
+        // gpio_set_level(ledG, 1);
+        // gpio_set_level(ledB, 1);
+        // vTaskDelay(pdMS_TO_TICKS(500));
+
+        // gpio_set_level(ledR, 0);
+        // gpio_set_level(ledG, 0);
+        // gpio_set_level(ledB, 0);
+        // vTaskDelay(pdMS_TO_TICKS(500));
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        printf("hello form main\n");
+    }
+}
+
+esp_err_t init_led()
+{
+    gpio_reset_pin(ledR);
+    gpio_set_direction(ledR, GPIO_MODE_OUTPUT);
+    gpio_reset_pin(ledG);
+    gpio_set_direction(ledG, GPIO_MODE_OUTPUT);
+    gpio_reset_pin(ledB);
+    gpio_set_direction(ledB, GPIO_MODE_OUTPUT);
+    return ESP_OK;
+}
+
+esp_err_t create_tasks(void)
+{
+    static uint8_t ucParameterToPass;
+    TaskHandle_t xHandle = NULL;
+    xTaskCreate( vTaskLedR, "vTaskLedR", STACK_SIZE, &ucParameterToPass, 1, &xHandle );
+    xTaskCreate( vTaskLedG, "vTaskLedG", STACK_SIZE, &ucParameterToPass, 1, &xHandle );
+    xTaskCreate( vTaskLedB, "vTaskLedB", STACK_SIZE, &ucParameterToPass, 1, &xHandle );
+    
+    return ESP_OK;
+}
+
+void vTaskLedR(void *pvParameters)
+{
+    for (;;)
+    {
+        // Task code goes here.
+        // Task code goes here.
+        ESP_LOGI(TAG, "Led R");
+        gpio_set_level(ledR, 1);
+        vTaskDelay(pdMS_TO_TICKS(R_DELAY));
+        gpio_set_level(ledR, 0);
+        vTaskDelay(pdMS_TO_TICKS(R_DELAY));
+
+    }
+}
+
+void vTaskLedG(void *pvParameters)
+{
+    for (;;)
+    {
+        // Task code goes here.
+        ESP_LOGI(TAG, "Led G");
+        gpio_set_level(ledG, 1);
+        vTaskDelay(pdMS_TO_TICKS(G_DELAY));
+        gpio_set_level(ledG, 0);
+        vTaskDelay(pdMS_TO_TICKS(G_DELAY));
+
+    }
+}
+
+void vTaskLedB(void *pvParameters)
+{
+    for (;;)
+    {
+        // Task code goes here.
+        // Task code goes here.
+        ESP_LOGI(TAG, "Led B");
+        gpio_set_level(ledB, 1);
+        vTaskDelay(pdMS_TO_TICKS(B_DELAY));
+        gpio_set_level(ledB, 0);
+        vTaskDelay(pdMS_TO_TICKS(B_DELAY));
+    }
+}
